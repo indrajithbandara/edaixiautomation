a31aa5ff069f65503aad86ee6bf45c551bcb56f8
Administrator <Administrator@172.31.9.23>
2015-09-14 06:58:06 +0000

    submit wechat end system codes for my automation

---
+++ src/uiwechatendtestcase/wechatend_testcase04_setting_managemenu.py
@@ -0,0 +1,103 @@
+#!/usr/lib/python2.7
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re,ConfigParser
+from selenium.webdriver.common.action_chains import ActionChains
+import appobjectops,ops_utiltools
+#Credit
+class OpsTestcase01Permissionmanagepermission(unittest.TestCase):
+    def setUp(self):
+        #self.driver = webdriver.Firefox()
+        self.driver = appobjectops.GetInstance()
+        self.driver.implicitly_wait(30)
+        conf = ConfigParser.ConfigParser()
+        conf.read(ops_utiltools.getopsconfigpath())
+        #conf.read("C:/edaixi_testdata/userdata_ops.conf")
+        global CAIWU_URL,USER_NAME,PASS_WORD
+        OPS_URL = conf.get("opssection", "uihostname")
+        USER_NAME = conf.get("opssection", "uiusername")
+        PASS_WORD = conf.get("opssection", "uipassword")
+        print OPS_URL,USER_NAME,PASS_WORD  
+        self.base_url = OPS_URL
+        #self.base_url = "http://ops05.edaixi.cn:81"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_ops_testcase01_permissionmanage_permission(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        #html body div#container.container h3.text-center.text-primary a.btn.btn-success.text-center
+        #driver.find_element_by_css_selector("div#container.container h3.text-center.text-primary a.btn.btn-success.text-center").click()
+        #loginclick=driver.find_element_by_css_selector("div#container.container h3.text-center.text-primary a.btn.btn-success.text-center")
+        loginclick=driver.find_element_by_css_selector(appobjectops.permloginClickButton)
+        ActionChains(driver).double_click(loginclick).perform()
+        driver.find_element_by_id("username").clear()
+        driver.find_element_by_id("username").send_keys(USER_NAME)
+        driver.find_element_by_id("password").clear()
+        driver.find_element_by_id("password").send_keys(PASS_WORD)
+        driver.find_element_by_id("login-submit").click()
+        print driver.title
+        self.assertEqual(driver.title, u"e袋洗城市运营后台")
+        time.sleep(2)
+        driver.find_element_by_css_selector(appobjectops.clickPermissionLink).click()
+        #permissionlinkclick=driver.find_element_by_css_selector("div.navbar.navbar-default.navbar-static-top div.container div.navbar-collapse.collapse.navbar-responsive-collapse ul.nav.navbar-nav li.dropdown a.dropdown-toggle")
+        #ActionChains(driver).double_click(permissionlinkclick).perform()
+        #driver.find_element_by_css_selector("div.navbar.navbar-default.navbar-static-top div.container div.navbar-collapse.collapse.navbar-responsive-collapse ul.nav.navbar-nav li.dropdown a.dropdown-toggle").click()
+        #driver.find_element_by_css_selector("div.navbar.navbar-default.navbar-static-top div.container div.navbar-collapse.collapse.navbar-responsive-collapse ul.nav.navbar-nav li:first-child.dropdown ul.dropdown-menu li:first-child a").click()
+        #driver.find_element_by_link_text(u"权限管理").click()
+        #driver.find_element_by_css_selector("ul.nav.navbar-nav > li:first-child.dropdown > ul.dropdown-menu > li:first-child > a").send_keys(Keys.ENTER)
+        #ul.nav.navbar-nav li.dropdown ul.dropdown-menu li a
+        #permissionclick=driver.find_element_by_css_selector("ul.nav.navbar-nav > li:first-child.dropdown > ul.dropdown-menu > li:first-child a")
+        #ActionChains.click(permissionclick)
+        #ActionChains(driver)..sigle_click(permissionclick).perform()
+        #driver.find_element_by_link_text(u"权限管理").click()
+        self.assertEqual(driver.title, u"e袋洗城市运营后台")
+        driver.find_element_by_xpath("/html/body/div[1]/div/div/ul/li[1]/ul/li[1]/a").click()
+        self.assertEqual(driver.title, u"e袋洗城市运营后台")
+        #driver.find_element_by_css_selector("div#container.container div.panel.panel-primary div.panle-body table.table.table-striped tbody tr:first-child td:last-child div.btn-toolbar a.btn.btn-sm.btn-success").click()
+        driver.find_element_by_css_selector(appobjectops.clickPermissionButton).click()
+        
+        self.assertEqual(driver.title, u"e袋洗城市运营后台")
+        driver.find_element_by_id("worker_is_admin").click()
+        driver.find_element_by_id("worker_is_city_manager").click()
+        #定位到要右击的元素
+        #qqq =driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/div/div[3]/table/tbody/tr/td[2]")
+        #对定位到的元素执行鼠标右键操作
+        #ActionChains(driver).context_click(qqq).perform()
+        driver.find_element_by_css_selector(".btn.btn-info").click()
+
+        print "ops persmission manage assert title is ",driver.title
+        self.assertEqual(driver.title, u"e袋洗城市运营后台")
+        
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException, e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ src/uiwechatfronttestcase/wechat_front_utiltools.py
@@ -0,0 +1,264 @@
+# -*- coding: utf-8 -*-
+
+from time import strftime, localtime
+from datetime import timedelta, date
+import calendar
+import time,random
+import datetime
+import ConfigParser,MySQLdb
+
+
+ARR = (7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2)
+LAST = ('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2')
+
+def makeNewIdentifiedCardId():
+    u''' 随机生成新的18为身份证号码 '''
+    t = time.localtime()[0]
+    x = '%02d%02d%02d%04d%02d%02d%03d' %(random.randint(10,99),
+                                        random.randint(01,99),
+                                        random.randint(01,99),
+                                        random.randint(t - 80, t - 18),
+                                        random.randint(1,12),
+                                        random.randint(1,28),
+                                        random.randint(1,999))
+    y = 0
+    for i in range(17):
+        y += int(x[i]) * ARR[i]
+
+    return '%s%s' %(x, LAST[y % 11])
+
+
+conf = ConfigParser.ConfigParser()
+conf.read("C:/edaixi_testdata/userdata_wuliu.conf")
+
+global CAIWU_URL,USER_NAME,PASS_WORD,mysqlhostname,mysqlusername,mysqlpassword,mysqldatabase
+mysqlhostname = conf.get("databaseconn", "mysqlhostname")
+mysqlusername = conf.get("databaseconn", "mysqlusername")
+mysqlpassword = conf.get("databaseconn", "mysqlpassword")
+mysqldatabase = conf.get("databaseconn", "mysqlrongchangdb")
+mysqlrongchangdb  = conf.get("databaseconn", "mysqlrongchangdb")
+print mysqlhostname, mysqlusername, mysqlpassword, mysqldatabase
+
+conn=MySQLdb.connect(host=mysqlhostname,user=mysqlusername,passwd=mysqlpassword,db=mysqldatabase,charset="utf8")    
+global cursor
+global ordersnnumber,fanidnumber,fansfanidnumber
+cursor = conn.cursor()
+
+def getordersn():
+   n = cursor.execute("SELECT ordersn FROM ims_washing_order WHERE status_delivery=3 AND fanxidan_id=0 AND bagsn IS NOT NULL  AND id=(SELECT MIN(id) FROM ims_washing_order) ORDER BY id") 
+   for row in cursor.fetchall():
+      for ordersn in row: 
+           return ordersn
+
+    #return ordersn
+#sglobal ordersn 
+#print getordersn()           
+#global ordersnnumber
+#ordersnnumber=str(getordersn())
+#keep database testdb has order number 15051110387266
+ordersnnumber=str(15051110387266)
+print "the random ordersn  is ",ordersnnumber  
+#print ordersn
+def getfanid():
+    n = cursor.execute("SELECT fan_id FROM ims_washing_order WHERE status_delivery=3 AND fanxidan_id=0 AND bagsn IS NOT NULL  AND id=(SELECT MIN(id) FROM ims_washing_order) ORDER BY id") 
+    for row in cursor.fetchall():
+      for fanid in row: 
+          return fanid  
+      
+#print gethuiyuanid()  
+fanidnumber=str(getfanid())
+print fanidnumber
+
+
+
+def getfanstablefanid():
+    n = cursor.execute("SELECT MIN(id)  FROM ims_fans") 
+    for row in cursor.fetchall():
+      for fanid in row: 
+          return fanid  
+fansfanidnumber=str(getfanstablefanid())
+print " the fansfanidnumber is ",fansfanidnumber  
+      
+      
+def getcloseconn():
+   if cursor!="":
+     cursor.close()
+   elif conn!="":
+     conn.close()
+     
+
+global signbagnumber
+signbagnumber="E0000000006"
+def updateSignNumber():
+    cursor = conn.cursor()    
+    cursor.execute("UPDATE ims_washing_order SET status_delivery='1',qianshoudian_id= NULL WHERE bagsn='E0000000006'")
+    conn.commit()
+        
+        
+year = strftime("%Y",localtime())
+mon  = strftime("%m",localtime())
+day  = strftime("%d",localtime())
+hour = strftime("%H",localtime())
+min  = strftime("%M",localtime())
+sec  = strftime("%S",localtime())
+
+def today():
+    '''''
+    get today,date format="YYYY-MM-DD"
+    '''''
+    return date.today()
+
+def todaystr():
+    '''
+    get date string, date format="YYYYMMDD"
+    '''
+    return year+mon+day
+
+def datetime():
+    '''''
+    get datetime,format="YYYY-MM-DD HH:MM:SS"
+    '''
+    return strftime("%Y-%m-%d %H:%M:%S",localtime())
+
+def datetimestr():
+    '''''
+    get datetime string
+    date format="YYYYMMDDHHMMSS"
+    '''
+    return year+mon+day+hour+min+sec
+
+def get_day_of_day(n=0):
+    '''''
+    if n>=0,date is larger than today
+    if n<0,date is less than today
+    date format = "YYYY-MM-DD"
+    '''
+    if(n<0):
+        n = abs(n)
+        return date.today()-timedelta(days=n)
+    else:
+        return date.today()+timedelta(days=n)
+
+def get_days_of_month(year,mon): 
+    ''''' 
+    get days of month 
+    ''' 
+    return calendar.monthrange(year, mon)[1] 
+  
+def get_firstday_of_month(year,mon): 
+    ''''' 
+    get the first day of month 
+    date format = "YYYY-MM-DD" 
+    ''' 
+    days="01" 
+    if(int(mon)<10): 
+        mon = "0"+str(int(mon)) 
+    arr = (year,mon,days) 
+    return "-".join("%s" %i for i in arr) 
+  
+def get_lastday_of_month(year,mon): 
+    ''''' 
+    get the last day of month 
+    date format = "YYYY-MM-DD" 
+    ''' 
+    days=calendar.monthrange(year, mon)[1] 
+    mon = addzero(mon) 
+    arr = (year,mon,days) 
+    return "-".join("%s" %i for i in arr) 
+  
+def get_firstday_month(n=0): 
+    ''''' 
+    get the first day of month from today 
+    n is how many months 
+    ''' 
+    (y,m,d) = getyearandmonth(n) 
+    d = "01" 
+    arr = (y,m,d) 
+    return "-".join("%s" %i for i in arr) 
+  
+def get_lastday_month(n=0): 
+    ''''' 
+    get the last day of month from today 
+    n is how many months 
+    ''' 
+    return "-".join("%s" %i for i in getyearandmonth(n)) 
+ 
+def getyearandmonth(n=0): 
+    ''''' 
+    get the year,month,days from today 
+    befor or after n months 
+    ''' 
+    thisyear = int(year) 
+    thismon = int(mon) 
+    totalmon = thismon+n 
+    if(n>=0): 
+        if(totalmon<=12): 
+            days = str(get_days_of_month(thisyear,totalmon)) 
+            totalmon = addzero(totalmon) 
+            return (year,totalmon,days) 
+        else: 
+            i = totalmon/12 
+            j = totalmon%12 
+            if(j==0): 
+                i-=1 
+                j=12 
+            thisyear += i 
+            days = str(get_days_of_month(thisyear,j)) 
+            j = addzero(j) 
+            return (str(thisyear),str(j),days) 
+    else: 
+        if((totalmon>0) and (totalmon<12)): 
+            days = str(get_days_of_month(thisyear,totalmon)) 
+            totalmon = addzero(totalmon) 
+            return (year,totalmon,days) 
+        else: 
+            i = totalmon/12 
+            j = totalmon%12 
+            if(j==0): 
+                i-=1 
+                j=12 
+            thisyear +=i 
+            days = str(get_days_of_month(thisyear,j)) 
+            j = addzero(j) 
+            return (str(thisyear),str(j),days) 
+  
+def addzero(n): 
+    ''''' 
+    add 0 before 0-9 
+    return 01-09 
+    ''' 
+    nabs = abs(int(n)) 
+    if(nabs<10): 
+        return "0"+str(nabs) 
+    else: 
+        return nabs 
+
+def get_today_month(n=0): 
+    ''''' 
+    获取当前日期前后N月的日期
+    if n>0, 获取当前日期前N月的日期
+    if n<0, 获取当前日期后N月的日期
+    date format = "YYYY-MM-DD" 
+    ''' 
+    (y,m,d) = getyearandmonth(n) 
+    arr=(y,m,d) 
+    if(int(day)<int(d)): 
+        arr = (y,m,day) 
+    return "-".join("%s" %i for i in arr) 
+  
+def getwuliuconfigpath():
+#window path    return "C:/edaixi_testdata/userdata_kefu.conf"
+    return "C:/edaixi_testdata/userdata_wuliu.conf"
+
+
+if __name__=="__main__":
+    start = time.clock()
+        
+    print today()  
+    print todaystr()
+    print datetime()
+    print datetimestr()
+    print get_day_of_day(1)
+    print get_day_of_day(-3)
+    print get_today_month(-3)
+    print get_today_month(3)
